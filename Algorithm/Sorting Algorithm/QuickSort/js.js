fdaf2ed200f3c791954749e84f30b6e1ea578c3d
Rakib_Penta <16103030@iubat.edu>
2020-08-15 16:14:22 +0600

    Quick sort done

--- Algorithm/Sorting Algorithm/QuickSort/js.js
+++ Algorithm/Sorting Algorithm/QuickSort/js.js
@@ -1,71 +1,46 @@
-let i = 0;
 let values = [];
-let changingVal = null;
-
 
 function setup(){
-    createCanvas(window.innerWidth, window.innerHeight);
-    frameRate(20);
-    for(let i = 0; i < width / 10; i++){
-        values.push(random(height / 1.5))
+    createCanvas(window.innerWidth, window.innerHeight)
+    for(let i = 0; i < width / 10; i ++) {
+        values.push(random(height / 1.4))
     }
-    quickSort(values, 0, values.length)
 }
 
 function draw(){
     background('white');
     values.forEach((item, index) => {
-        if(index === changingVal) {
-            fill('red');
-            stroke('white');
-            rect(index * 10, height / 1.4, 10, -item)
-        } else {
-            fill('#555');
-            stroke('white');
-            rect(index * 10, height / 1.4, 10, -item)
-        }
+        fill('#555');
+        stroke('white');
+        rect(index * 10, height / 1.4, 10, -item)
     });
-    // if(i < values.length){
-    //     let j = i;
-    //     let temp = values[j];
-    //     while(j > 0 && temp < values[j - 1]){
-    //         changingVal = j - 1;
-    //         values[j] = values[j - 1];
-    //         j = j - 1
-    //     }
-    //     console.log(values)
-    //     values[j] = temp;
-    //     i++;
-    // } else {
-    //     changingVal = null
-    // }
-}
 
+    quickSort(values, 0, values.length)
+}
 
 function quickSort(arr, start, end){
     if(start < end){
-        let pivPos = partition(arr, start, end);
-        quickSort(arr,start , pivPos -1);
-        quickSort( arr,pivPos +1 , end);
-    }
+        let pivotPosition = partition(arr, start, end);
+        quickSort(arr, start, pivotPosition - 1);
+        quickSort(arr, pivotPosition + 1, end);
+    } else return
 }
 
 function partition(arr, start, end){
-    let i = start + 1;
-    let piv = arr[start];
-    for(let j = start + 1; j <= end ; j++ )  {
-    
-              if ( arr[ j ] < piv) {
-                     swap (arr, arr[ i ],arr [ j ]);
-                i += 1;
-            }
-       }
-    swap ( arr[ start ] ,arr[ i-1 ] ) ;
-    return i-1;
+    let pivotIndex = start;
+    let pivotValue = arr[end];
+    for(let j = start; j < end; j++){
+        if(arr[j] < pivotValue){
+            swap(arr, j, pivotIndex);
+            pivotIndex++;
+        }
+    }
+    swap(arr,pivotIndex, end);
+    return pivotIndex; 
 }
 
-let swap = function(arr, firstInd, secInd){
+function swap(arr, firstInd, secondInd ){
     let holdFirstVal = arr[firstInd];
-    arr[firstInd] = arr[secInd];
-    arr[secInd] = holdFirstVal;
+    arr[firstInd] = arr[secondInd];
+    arr[secondInd] = holdFirstVal;
 }
