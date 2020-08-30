3dca3587dd2b42a0374011e19006d93a4f17fdc4
Rakib_Penta <16103030@iubat.edu>
2020-08-30 18:25:29 +0600

    BFS initialization

--- Algorithm/Sorting Algorithm/QuickSort/js.js
+++ Algorithm/Sorting Algorithm/QuickSort/js.js
@@ -2,7 +2,7 @@ let values = [];
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight)
-    for(let i = 0; i < width / 10; i ++) {
+    for(let i = 0; i < width / 100; i ++) {
         values.push(random(height / 1.4))
     }
 }
@@ -12,35 +12,40 @@ function draw(){
     values.forEach((item, index) => {
         fill('#555');
         stroke('white');
-        rect(index * 10, height / 1.4, 10, -item)
+        rect(index * 100, height / 1.4, 100, -item)
     });
 
-    quickSort(values, 0, values.length)
+    quickSort(values, 0, values.length - 1)
 }
 
-function quickSort(arr, start, end){
+async function quickSort(arr, start, end){
     if(start < end){
-        let pivotPosition = partition(arr, start, end);
-        quickSort(arr, start, pivotPosition - 1);
-        quickSort(arr, pivotPosition + 1, end);
+        let pivotPosition = await partition(arr, start, end);
+        await quickSort(arr, start, pivotPosition - 1);
+        await quickSort(arr, pivotPosition + 1, end);
     } else return
 }
 
-function partition(arr, start, end){
+async function partition(arr, start, end){
     let pivotIndex = start;
     let pivotValue = arr[end];
     for(let j = start; j < end; j++){
         if(arr[j] < pivotValue){
-            swap(arr, j, pivotIndex);
+            await swap(arr, j, pivotIndex);
             pivotIndex++;
         }
     }
-    swap(arr,pivotIndex, end);
+    await timeout(500)
+    await swap(arr,pivotIndex, end);
     return pivotIndex; 
 }
 
-function swap(arr, firstInd, secondInd ){
+async function swap(arr, firstInd, secondInd ){
     let holdFirstVal = arr[firstInd];
     arr[firstInd] = arr[secondInd];
     arr[secondInd] = holdFirstVal;
+}
+
+async function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
