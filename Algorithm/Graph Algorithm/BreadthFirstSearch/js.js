c048bbd96666a78665777a4d93824e8778ff02b3
Rakib_Penta <16103030@iubat.edu>
2020-09-04 12:45:03 +0600

    BFS

--- Algorithm/Graph Algorithm/BreadthFirstSearch/js.js
+++ Algorithm/Graph Algorithm/BreadthFirstSearch/js.js
@@ -6,3 +6,16 @@ function setup(){
 function draw(){
 
 }
+
+
+function Node(value){
+    this.value = value;
+    this.edges = [];
+    this.searched = false;
+    this.parent = null;
+}
+
+
+function Graph(){
+
+}
