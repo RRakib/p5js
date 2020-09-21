c3465a954221cb2e932cfed4d261fd629dce9184
Rakib_Penta <16103030@iubat.edu>
2020-09-21 22:42:40 +0600

    Labelling

--- ImageLabelling/js.js
+++ ImageLabelling/js.js
@@ -1,11 +1,12 @@
 let img;
 
 function preload(){
-    
+    img = loadImage('./ImageFiltering/test.jpg');
 }
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
+    image(img, 0, 0);
 }
 
 function draw(){
