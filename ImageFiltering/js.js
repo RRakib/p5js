b87c2765ab49120acca463e2228f383ce6484c67
Rakib_Penta <16103030@iubat.edu>
2020-10-10 16:40:39 +0600

    Image filtering

--- ImageFiltering/js.js
+++ ImageFiltering/js.js
@@ -3,7 +3,7 @@
 let img, c;
 
 function preload(){
-    img = loadImage('./ImageFiltering/test.jpg');
+    img = loadImage('./ImageFiltering/test.jpeg');
 }
 
 function setup(){
@@ -21,10 +21,29 @@ function draw(){
             let r = loc + 0
             let g = loc + 1
             let b = loc + 2
-            let brightness = (r+g+b) / 3
-            pixels[loc + 0] = img.pixels[brightness];
-            pixels[loc + 1] = img.pixels[brightness];
-            pixels[loc + 2] = img.pixels[brightness];
+            let brightness = (r+g+b) / 3;
+            let imageR = img.pixels[r]
+            let imageG = img.pixels[g]
+            let imageB = img.pixels[b]
+            let imgPix = img.pixels[brightness]
+
+            let contrast = (-50/100) + 1;  //convert to decimal & shift range: [0..2]
+            let intercept = 128 * (1 - contrast);
+
+            pixels[loc + 0] = imageR + (imageR - intercept);
+            pixels[loc + 1] = imageG + (imageG - intercept);
+            pixels[loc + 2] = imageB + (imageB - intercept);
+            // if(img.pixels[loc + 3] <= 100){
+            //     pixels[loc + 0] = img.pixels[r] + img.pixels[loc + 3];
+            //     pixels[loc + 1] = img.pixels[g] + img.pixels[loc + 3];
+            //     pixels[loc + 2] = img.pixels[b] + img.pixels[loc + 3];
+            //     pixels[loc + 3] = 255;
+            // } else {
+                // pixels[loc + 0] = img.pixels[r];
+                // pixels[loc + 1] = img.pixels[g];
+                // pixels[loc + 2] = img.pixels[b];
+                // pixels[loc + 3] = img.pixels[loc + 3];
+            // }
         }
     }
     updatePixels()
